004d03565116f3a353ea0216b872bd74fe9e88a0
Adam Boros <hunaboros@gmail.com>
2013-04-15 11:22:19 +0200

    Change page title to CEU Faculty Experts.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head lang="en" xml:lang="en">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <meta name="language" content="english" />
-	<title>CEU Experts</title>
+	<title>CEU Faculty Experts</title>
   <style type="text/css" title="currentStyle">
   @import "stylesheets/table.css";
   @import "stylesheets/jquery.dataTables.css";
@@ -69,7 +69,7 @@
 <body>
   <div id="page">
     <div id="header">
-      <h1>CEU Experts</h1>
+      <h1>CEU Faculty Experts</h1>
     </div>
     <div id="description">
       <p>At vero eos et accusam et justo duo dolores et ea rebum. <a href="#">Stet clita kasd gubergren, no sea takimata</a> sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
